bf990160852ed08b33f09de89e46ebd5ec465e42
jcnventura <jcnventura@122464.no-reply.drupal.org>
2020-08-22 17:37:10 +0200

    Issue #2978265 by jcnventura, artematem, Manuel Garcia, daggerhart, jhedstrom, nerdstein, andypost, harshil.maradiya, wturrell, baikho: Default encryption method mcrypt is deprecated since PHP7.1

--- src/Plugin/EncryptionMethod/McryptAES128Encryption.php
+++ src/Plugin/EncryptionMethod/McryptAES128Encryption.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\tfa\Plugin\EncryptionMethod;
 
+use Drupal\Component\Serialization\Json;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\encrypt\EncryptionMethodInterface;
 use Drupal\encrypt\Plugin\EncryptionMethod\EncryptionMethodBase;
@@ -14,7 +15,7 @@ use Drupal\encrypt\Plugin\EncryptionMethod\EncryptionMethodBase;
  * @EncryptionMethod(
  *   id = "mcrypt_aes_128",
  *   title = @Translation("Mcrypt AES 128"),
- *   description = "This uses PHPs mcrypt extension and <a href='http://en.wikipedia.org/wiki/Advanced_Encryption_Standard'>AES-128</a>.",
+ *   description = "This uses PHP OpenSSL or Mcrypt extensions and <a href='http://en.wikipedia.org/wiki/Advanced_Encryption_Standard'>AES-128</a>.",
  *   key_type = {"encryption"},
  *   can_decrypt = TRUE
  * )
@@ -22,48 +23,110 @@ use Drupal\encrypt\Plugin\EncryptionMethod\EncryptionMethodBase;
 class McryptAES128Encryption extends EncryptionMethodBase implements EncryptionMethodInterface {
   use StringTranslationTrait;
 
-  /**
-   * @return mixed
-   */
-  public function encrypt($text, $key, $options = []) {
-    $processed_text = '';
+  const CRYPT_VERSION = 1;
 
+  /**
+   * {@inheritdoc}
+   */
+  public function encrypt($text, $key) {
+    // Backwards compatibility with Mcrypt.
+    if (!extension_loaded('openssl') && extension_loaded('mcrypt')) {
+      return $this->encryptWithMcrypt($text, $key);
+    }
+
+    // Encrypt using OpenSSL.
+    $iv = random_bytes(16);
+    $ciphertext = openssl_encrypt($text, 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv);
+    $crypto_data = [
+      'version' => self::CRYPT_VERSION,
+      'iv_base64' => base64_encode($iv),
+      'ciphertext_base64' => base64_encode($ciphertext),
+    ];
+    return Json::encode($crypto_data);
+  }
+
+  /**
+   * Encrypt using the deprecated Mcrypt extension.
+   *
+   * @param string $text
+   *   The text to be encrypted.
+   * @param string $key
+   *   The key to encrypt the text with.
+   *
+   * @return string
+   *   The encrypted text.
+   *
+   * @noinspection PhpDeprecationInspection
+   */
+  private function encryptWithMcrypt($text, $key) {
     // Key cannot be too long for this encryption.
     $key = mb_substr($key, 0, 32);
 
     // Define iv cipher.
     $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_ECB);
     $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
-    $disable_base64 = array_key_exists('base64', $options) && $options['base64'] == FALSE;
 
     $processed_text = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $text, MCRYPT_MODE_ECB, $iv);
-
-    // Check if we are disabling base64 encoding.
-    if (!$disable_base64) {
-      $processed_text = base64_encode($processed_text);
-    }
+    $processed_text = base64_encode($processed_text);
 
     return $processed_text;
   }
 
   /**
-   * @return mixed
+   * {@inheritdoc}
    */
-  public function decrypt($text, $key, $options = []) {
-    $processed_text = '';
+  public function decrypt($text, $key) {
+    $crypto_data = Json::decode($text);
+    if (empty($crypto_data['version']) || empty($crypto_data['iv_base64']) || empty($crypto_data['ciphertext_base64'])) {
+      // Backwards compatibility with the old Mcrypt scheme.
+      return extension_loaded('mcrypt') ? $this->decryptLegacyDataWithMcrypt($text, $key) : $this->decryptLegacyDataWithOpenSSL($text, $key);
+    }
+    else {
+      $iv = base64_decode($crypto_data['iv_base64']);
+      $ciphertext = base64_decode($crypto_data['ciphertext_base64']);
+      return openssl_decrypt($ciphertext, 'aes-256-cbc', $key, TRUE, $iv);
+    }
+  }
 
+  /**
+   * Use OpenSSL to decrypt data that was originally encrypted with Mcrypt.
+   *
+   * @param string $text
+   *   The text to be decrypted.
+   * @param string $key
+   *   The key to decrypt the text with.
+   *
+   * @return string|bool
+   *   The decrypted text, or FALSE on failure.
+   */
+  private function decryptLegacyDataWithOpenSsl($text, $key) {
+    $key = mb_substr($key, 0, 32);
+    $text = base64_decode($text);
+
+    return openssl_decrypt($text, 'aes-128-cbc', $key, OPENSSL_NO_PADDING);
+  }
+
+  /**
+   * Decrypt using the deprecated Mcrypt extension.
+   *
+   * @param string $text
+   *   The text to be decrypted.
+   * @param string $key
+   *   The key to decrypt the text with.
+   *
+   * @return string
+   *   The decrypted text
+   *
+   * @noinspection PhpDeprecationInspection
+   */
+  private function decryptLegacyDataWithMcrypt($text, $key) {
     // Key cannot be too long for this encryption.
     $key = mb_substr($key, 0, 32);
 
     // Define iv cipher.
     $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_ECB);
     $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
-    $disable_base64 = array_key_exists('base64', $options) && $options['base64'] == FALSE;
-
-    // Check if we are disabling base64 encoding.
-    if (!$disable_base64) {
-      $text = base64_decode($text);
-    }
+    $text = base64_decode($text);
 
     // Decrypt text.
     return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $text, MCRYPT_MODE_ECB, $iv));
@@ -83,8 +146,8 @@ class McryptAES128Encryption extends EncryptionMethodBase implements EncryptionM
   public function checkDependencies($text = NULL, $key = NULL) {
     $errors = [];
 
-    if (!function_exists('mcrypt_encrypt')) {
-      $errors[] = $this->t('MCrypt library not installed.');
+    if (!extension_loaded('openssl') && !extension_loaded('mcrypt')) {
+      $errors[] = $this->t('OpenSSL and Mcrypt extensions are not installed.');
     }
 
     // Check if we have a 128 bit key.
